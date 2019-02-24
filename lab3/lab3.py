581050327386956daa62459a5626ad3a32bdd52e
joiemoie <joiemoie2004@gmail.com>
2019-02-23 18:25:14 -0800

    fixed lateral inhibition and put it in layer.py

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -7,7 +7,7 @@ from filters import OffCenterFilter, OnCenterFilter
 import csv
 
 # Lateral inhibition window size
-LI_WINDOW = 1
+LI_WINDOW = 3
 
 mnist = fetch_mldata('MNIST original')
 N, _ = mnist.data.shape
@@ -16,61 +16,16 @@ N, _ = mnist.data.shape
 mnist.square_data = mnist.data.reshape(N,28,28)
 
 
-def wta(spikes, t, num_winner, winner_index):
-    '''
-    Performs Winner-Take-All inhibition on the spikes
-    t is the current time step
-    num_winner is the number of winners we will let pass the inhibition
-    winner_index is the previous winning index
-    '''
-    out = np.zeros(spikes.shape)
-    out[:] = -1
-    winner = 0
-    index = []
-
-    # Need to generate new winner
-    if t % LI_WINDOW == 0:
-        index = []
-        for i in range(spikes.shape[0]):
-            if winner != num_winner and spikes[i] == 0:
-                out[i] = 0
-                index.append(i)
-                winner += 1
-    else:
-        index = winner_index
-        # Take the spikes from the winning neurons
-        for i in winner_index:
-            out[i] = spikes[i]
-    
-    return (out, index)
-
-
 # Structure of the TNN
 layer1 = firstlayer.FirstLayer(layer_id=1, training_raw_data=mnist.square_data[0], threshold=8, inhibit_k=3)
 layer2 = layer.Layer(layer_id=2, num_neurons=16, prev_layer=layer1, threshold=3)
 num_iterations = 9
 
-''' Test for WTA
-spikes = []
-spikes.append(np.array([0, -1, 0, 0]))
-spikes.append(np.array([-1, -1, 0, 0]))
-spikes.append(np.array([0, -1, -1, 0]))
-spikes.append(np.array([-1, -1, -1, 0]))
-spikes.append(np.array([-1, -1, -1, 0]))
-
-li_index = []
-
-for t in range(5):
-    print("spikes for LI: ", spikes[t])
-    (li_spikes, li_index) = wta(spikes[t], t, 1, li_index)
-    print(li_spikes, li_index)
-'''
-
 with open('spiketimes.csv', 'w', newline='') as csvfile:
   writer = csv.writer(csvfile)
 
   # Generates spikes for layer 1 using 2 different filters
-  for i in range(len(mnist.square_data)):
+  for i in range(len(mnist.square_data[:2])):
     layer1.raw_data = mnist.square_data[i]
     layer1.generate_spikes(OnCenterFilter, OffCenterFilter)
 
@@ -78,14 +33,14 @@ with open('spiketimes.csv', 'w', newline='') as csvfile:
 
     for j in range(num_iterations):
       layer2.generate_spikes()
-      (li_spikes, li_index) = wta(layer2.spikes, j, 1, li_index)
+      (li_spikes, li_index) = layer2.wta(1, LI_WINDOW)
 
       for k in range(layer2.spikes.shape[0]):
         if (li_spikes[k] == 0):
           image_number = i
           spike_position = k
           spike_time = j
-          #print([image_number, spike_position, spike_time])
+          print([image_number, spike_position, spike_time])
           writer.writerow([image_number, spike_position, spike_time])
   
       layer1.increment_time()
