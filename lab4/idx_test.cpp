9fe148584e68b46b903d0dd4459f2a666a7c517a
Jamal <Jamal.Ra.Davis@gmail.com>
2019-03-19 21:27:56 -0700

    commiting idx_test

--- lab4/idx_test.cpp
+++ lab4/idx_test.cpp
@@ -6,20 +6,27 @@
 
 void getIndexes(int N)
 {
+    int layer_idx = 0;
     for (int i=N/2; i>0; i /= 2)//Layers
     {
         int gpl = (N/2)/i;
+        int stride = i;
+
         for (int j=0; j<gpl; j++)//Groups per layer
         {
+            int beginning = j * (2*stride);
             int bpg = (N/2)/gpl;
             //printf("--i: %d, j: %d\n", i, j);
-            for (int k=0; k<bpg; k++)
+            for (int k=0; k<i; k++)
             {
-                printf("i: %d, j: %d, k: %d\n", i, j, k);
+                int idx_start = beginning + k;
+                int idx_end = idx_start + stride;
+                printf("i: %d, j: %d, k: %d, layer_idx: %d, idx_start: %d, idx_end: %d\n", i, j, k, layer_idx, idx_start, idx_end);
             }
             printf("\n");
         }
         printf("\n");
+        layer_idx++;
     }
 
 
