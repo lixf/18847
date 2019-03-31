c15f657a5e6881748aa22ae6ab7433e789604e64
joiemoie <joiemoie2004@gmail.com>
2019-03-31 16:18:53 -0700

    Added test data

--- Final-Project/network.py
+++ Final-Project/network.py
@@ -62,17 +62,18 @@ def evaluate(layer1, layer2, data, target, receptive_field, parameters=None, isT
     return test_results
 
 
-def calculate_metrics(data, target, receptive_field, parameters=None, num_data=2000):
+def calculate_metrics(data, target, receptive_field_length, threshold, parameters=None, num_data=2000, isForced=False):
 
   # Structure of the TNN
 
   num_outputs = 10
 
   #threshold indicates the highest filter spiketime that can be condsidered
-  layer1 = firstlayer.FirstLayer(layer_id=1, training_raw_data=data[0], threshold=8, receptive_field_length = 28)
+  layer1 = firstlayer.FirstLayer(layer_id=1, training_raw_data=data[0], threshold=8, receptive_field_length=receptive_field_length)
+  receptive_field = (int(14-receptive_field_length/2),int(14-receptive_field_length/2))
 
   # threshold indicates the max neuron sum before firing
-  layer2 = layer.Layer(layer_id=2, num_neurons=num_outputs, prev_layer=layer1, threshold=1000)
+  layer2 = layer.Layer(layer_id=2, num_neurons=num_outputs, prev_layer=layer1, threshold=threshold)
 
   # selects 10000 random images for training and testing
   permutation = np.random.permutation(len(data))
@@ -82,7 +83,7 @@ def calculate_metrics(data, target, receptive_field, parameters=None, num_data=2
   # Generates spikes for layer 1 using 2 different filters
   # this is the testing phase
 
-  training_results, assignments = evaluate(layer1, layer2, data[training], target[training], receptive_field, parameters, True, None, True)
+  training_results, assignments = evaluate(layer1, layer2, data[training], target[training], receptive_field, parameters, True, None, isForced)
   print(assignments)
   test_results = evaluate(layer1, layer2, data[test], target[test], receptive_field,parameters, False, assignments)
   return [training_results, test_results]
@@ -105,24 +106,59 @@ mnist.square_data = mnist.data.reshape(N,28,28)
 #axis=1)
 #np.savetxt("results1.csv", final_results1, delimiter=",")
 
-print("Receptive Field: (12,12)")
-input_output_weight = 1
-input_no_output_weight = .05
-no_input_output_weight = .2
-input_inhibited_output_weight = .05
-parameters = [input_output_weight, input_no_output_weight, input_inhibited_output_weight, no_input_output_weight]
-num_data = 4000
-training_results, test_results  = calculate_metrics(mnist.square_data, mnist.target, (0,0), parameters, num_data)
-coverage = np.sum(test_results[0]) / (num_data/2)
-print("Coverage: ", coverage)
-print("Purity: ", np.mean(np.amax(training_results, axis=1) / np.sum(training_results, axis=1)))
-print("Accuracy: ", np.mean(test_results[1] / test_results[0]))
+results_array = np.load('results.out.npy')
+print(results_array)
+for i in range(8,28):
+
+  range1 = range(10,int((i**2)*10*.75), 5)
+  range2 = range(20, int((i**2)*10*.75), 20)
+  range3 = range(100, min(int((i**2)*10*.75), 1200), 50)
+  selected_range = range1
+  if (i >= 4 and i < 8):
+    selected_range = range2
+  elif (i >=8):
+    selected_range = range3
+
+  for j in selected_range:
+    j_index = int((j - 10)/5)
+    if (i >=4 and i < 8):
+      j_index = int((j - 20)/20)
+    elif (i >= 8):
+      j_index = int((j-100)/50)
+
+    coverage = 0
+    for k in range(2):
+      print("Receptive Field: ", i)
+      print("Threshold: ", j)
+      print("J_index: ", j_index)
+      print("isForced", k == 0)
+      input_output_weight = 1
+      input_no_output_weight = .05
+      no_input_output_weight = .2
+      input_inhibited_output_weight = .05
+      parameters = [input_output_weight, input_no_output_weight, input_inhibited_output_weight, no_input_output_weight]
+      num_data = 4000
+      training_results, test_results  = calculate_metrics(mnist.square_data, mnist.target, i,j, parameters, num_data,k == 0)
+      coverage = np.sum(test_results[0]) / (num_data/2)
+      purity = np.mean(np.amax(training_results, axis=1) / np.sum(training_results, axis=1))
+      accuracy = np.mean(test_results[1] / test_results[0])
+      print("Coverage: ", coverage)
+      print("Purity: ", purity)
+      print("Accuracy: ", accuracy)
+      results_array[i, j_index, k, 0] = coverage
+      results_array[i, j_index, k, 1] = purity
+      results_array[i, j_index, k, 2] = accuracy
+
+      np.save('results.out', results_array)
+    if (coverage < 0.05):
+      break
 
 #print("Receptive Field: (12,16)")
 #results3 = calculate_metrics(mnist.square_data, mnist.target, (12,16))
 #max3 = np.transpose(np.asarray([np.amax(results3, axis=1)]))
 #totals3 = np.transpose(np.asarray([np.sum(results3, axis=1)]))
-#coverage3 = np.transpose(np.asarray([np.sum(results3, axis=1) / 10000]))
+#coverage3 = np.transpose(np.asarr
+#ay([np.sum(results3, axis=1) / 10000]))
 #purity3 = np.transpose(np.asarray([np.amax(results3, axis=1) / np.sum(results3, axis=1)]))
 #final_results3 = np.concatenate((results3,max3, totals3, coverage3, purity3), axis=1)
 #np.savetxt("results3.csv", final_results3, delimiter=",")
